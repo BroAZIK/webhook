ccf3064e92c8f4dc3bc05e2462a4d659bcb2c573
BroAZIK <tolliboyevazizbek21@gmail.com>
2023-10-07 10:14:28 +0500

    done

--- handlers.py
+++ handlers.py
@@ -3,6 +3,11 @@ from telegram.ext import CallbackContext
 
 
 def start(update: Update, context: CallbackContext):
+
+    print(update.effective_user.first_name)
+    print(update.message.text)
+    print("\n**************\n")
+
     update.message.reply_text(
         'Hello!, welcome to bot!'
     )
